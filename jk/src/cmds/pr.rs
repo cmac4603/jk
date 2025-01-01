35f7d9c8891943eff38a3730d1907c925ceb2920
cmac4603 <cmac4603@users.noreply.github.com>
2025-01-01 21:09:51 +0000

    github-pr-response
    Description:
    - Handle github errors...maybe

--- jk/src/cmds/pr.rs
+++ jk/src/cmds/pr.rs
@@ -30,13 +30,19 @@ impl PrCommand {
         let git_client = GitClient::new(cfg)?;
         let gh_client = GithubClient::default();
         let (org, repo) = git_client.get_org_repo()?;
+
+        // inputs from user
         let branch_name = Text::new("Branch name:").prompt()?;
         let commit_msg = pr_comment(commit_template)?;
-        git_client.add_all_commit(branch_name.clone(), commit_msg.clone())?;
-        git_client.push("create-pr".to_string())?;
-        let pr_data = PrData::new(branch_name.clone(), commit_msg, "main".to_string());
-        gh_client.create_pull_request(org, repo, pr_data).await?;
 
-        Ok("Ok".to_string())
+        // push to github
+        git_client.add_all_commit(branch_name.clone(), commit_msg.clone())?;
+        git_client.push(branch_name.clone())?;
+
+        // create pull request
+        let pr_data = PrData::new(branch_name.clone(), commit_msg, "main".to_string());
+        let gh_resp = gh_client.create_pull_request(org, repo, pr_data).await?;
+
+        Ok(gh_resp.html_url)
     }
 }
